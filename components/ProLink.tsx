0d6027a378bb14296cea37250652dd7ac4b57cc9
Stupnikjs <n.boudier.ph@gmail.com>
2022-12-29 19:23:09 +0100

    ok

--- components/ProLink.tsx
+++ components/ProLink.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
-import style from "./prolink.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import style from './prolink.module.css'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-const ProLink = () => {
+
+const Prolink = () => {
+    const [offset, setOffset] = useState(10)
+
+    useEffect(()=> {
+
+    const timer = () => {
+        setInterval(()=> {
+            setOffset( offset + 1)
+            console.log("hello")
+        }, 1000)
+    }
+    
+    
+
+    }, [offset])
+
     return (
-        <div className={style.prolink}>
-            <FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon>
-            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+        <div className={style.prolink} style={{marginLeft: `${offset}px`}}>
+            <Link href="https://github.com/Stupnikjs"><FontAwesomeIcon icon={faGithub}/> </Link>
+            <Link href="mailto:n.boudier.ph@gmail.com"> <FontAwesomeIcon icon={faEnvelope}/> </Link>
         </div>
     );
 };
 
-export default ProLink;
+export default Prolink;
--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,57 @@
 
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Headeur from './layout/Headeur'
-import Footer from "./layout/Footer"
-import ProLink from "../components/ProLink"
+import  React, { useState, useEffect } from 'react';
+
 import {Playfair_Display} from '@next/font/google'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
+import Prolink from "../components/Prolink"
+
 
 const playfair = Playfair_Display({ weight:"400", subsets: ['latin']})
 
+
+
 export default function Home() {
-
-   
-    const [scrollY, setScrollY] = useState(0); 
-    const [scrolled, setScrolled] = useState(false)
-    const [scrollTop, setScrollTop] = useState(false)
-    const [displayArrow, setDisplayArrow] = useState(false)
-
-    const scrollHandler= (e) => {
-
-        const {scrollY} = window;
-        const windowHeight = window.innerHeight 
-        setScrollY(scrollY)
-        if (scrollY > windowHeight) setDisplayArrow(true)
-        else setDisplayArrow(false)
-    }
-
 
 
     useEffect(() => {
-      
-        if (scrollY > 0) setScrolled(true)
-        if (scrollY === 0) setScrolled(false)
-
-        window.addEventListener("scroll", scrollHandler, {passive: true})
-        if (scrollTop){ 
-            window.scrollTo({ top: 0, behavior:"smooth"})
-            setScrollTop(false)
-        }
-        
-        
-
-        return () => {
-            window.removeEventListener("scroll", scrollHandler, { passive: true });
-         }
-
-    }, [scrollTop, scrollY]); 
+   
+    }, []); 
 
     return (
         <div className={playfair.className}>
-            <Headeur scrolled={scrolled} ></Headeur>
-            <main className='main_index'>
-                <div className='index_presentation'> 
-                    <h2> Bonjour, je m'appelle </h2>
-                    <h1><strong>Nicolas,</strong> Boudier </h1>
-                    <p>Developpeur <strong className='strong'>Javascript</strong> </p>
-                    
-                    <h2 > Construisons ensemble votre projet !</h2>
+            <main className="main-container">
+                <aside className='left-side'>
+                    <Prolink></Prolink>
+                </aside>
+                <div className="cards-div">
+                    <div className="index-card">
+                        <h1> Bonjour, </h1>
+                        <h2> Bienvenue sur mon Portfolio.</h2>
+                        <p>Je m'appelle Nicolas Boudier </p>
+                        <p> Je suis developpeur Web autodidacte <FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon> </p>
+                    </div>
+                    <div className="index-card">
+                        <h1> Bonjour, </h1>
+                        <h2> Bienvenue sur mon Portfolio.</h2>
+                        <p>Je m'appelle Nicolas Boudier </p>
+                        <p> Je suis developpeur Web autodidacte <FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon> </p>
+                    </div>
+                    <div className="index-card">
+                        <h1> Bonjour, </h1>
+                        <h2> Bienvenue sur mon Portfolio.</h2>
+                        <p>Je m'appelle Nicolas Boudier </p>
+                        <p> Je suis developpeur Web autodidacte <FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon> </p>
+                    </div>
+                    <div className="index-card">
+                        <h1> Bonjour, </h1>
+                        <h2> Bienvenue sur mon Portfolio.</h2>
+                        <p>Je m'appelle Nicolas Boudier </p>
+                        <p> Je suis developpeur Web autodidacte <FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon> </p>
+                    </div>
                 </div>
-                <button className='contact_button'>contactez moi</button>
-                <ProLink></ProLink>
+                
             </main>
-            <Footer></Footer>
         </div>
     ) 
 
